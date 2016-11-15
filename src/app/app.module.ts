48f97cfcdf2a1724ea521094b3da44262288d580
Kiana-Aghaei <kiana.aghaei@gmail.com>
2016-11-15 14:49:12 -0800

    up and running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RegisterComponent } from './register/register.component';
+import { EncounterComponent } from './encounter/encounter.component';
+import { ReportComponent } from './report/report.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ServicesComponent } from './services/services.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'encounter', component: EncounterComponent },
+  { path: 'report', component: ReportComponent },
+
+  { path: '**', component: NotfoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    EncounterComponent,
+    ReportComponent,
+    NotfoundComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Colonist, Job } from '../models';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+colonist: Colonist;
+marsJobs: [Job];
+
+  constructor() {
+    this.colonist = new Colonist('', null, null, null);
+  }
+
+  ngOnInit() {
+  }
+
+}
